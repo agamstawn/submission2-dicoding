149f23270847ccd3f2d62089af42817683642ce9
Agam Setiawan <agam.setiawan@wgs.co.id>
2019-05-01 14:07:38 +0700

    test submission2

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
-<?php
-if(isset($_POST['btn-upload']))
-{
-	$img = rand(1000,100000)."-".$_FILES['img']['name'];
-	$img_loc = $_FILES['img']['tmp_name'];
-	$folder="uploads/";
-	if(move_uploaded_file($img_loc,$folder.$img))
-	{
-		echo "<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-wp-preserve="%3Cscript%3Ealert('Upload%20Sukses!!!')%3B%3C%2Fscript%3E" data-mce-resize="false" data-mce-placeholder="1" class="mce-object" width="20" height="20" alt="<script>" title="<script>" />";
-	}
-	else
-	{
-		echo "<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-wp-preserve="%3Cscript%3Ealert('Upload%20Gagal')%3B%3C%2Fscript%3E" data-mce-resize="false" data-mce-placeholder="1" class="mce-object" width="20" height="20" alt="<script>" title="<script>" />";
-	} 
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>

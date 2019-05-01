745aa8581ccf5b0417fd29f32287d11024b5f426
Agam Setiawan <agam.setiawan@wgs.co.id>
2019-05-01 16:56:32 +0700

    try storage add try

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('ACCOUN
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 
 // // $fileToUpload = "HelloWorld.txt";
-// $fileToUpload = rand(1000,100000)."-".$_FILES['img']['name'];
+$fileToUpload = $_FILES;
 
 
 if(isset($_POST['btn-upload']))
@@ -31,77 +31,77 @@ if(isset($_POST['btn-upload']))
 
     $containerName = "blockblobs".generateRandomString();
 
-//     try {
-//         // Create container.
-//         $blobClient->createContainer($containerName, $createContainerOptions);
+    try {
+        // Create container.
+        $blobClient->createContainer($containerName, $createContainerOptions);
 
-//         // Getting local file so that we can upload it to Azure
-//         $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
-//         fclose($myfile);
+        // Getting local file so that we can upload it to Azure
+        $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
+        fclose($myfile);
         
-//         # Upload file as a block blob
-//         echo "Uploading BlockBlob: ".PHP_EOL;
-//         echo $fileToUpload;
-//         echo "<br />";
+        # Upload file as a block blob
+        echo "Uploading BlockBlob: ".PHP_EOL;
+        echo $fileToUpload;
+        echo "<br />";
         
-//         $content = fopen($fileToUpload, "r");
+        $content = fopen($fileToUpload, "r");
 
-//         //Upload blob
-//         $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+        //Upload blob
+        $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
 
-//         // List blobs.
-//         $listBlobsOptions = new ListBlobsOptions();
-//         $listBlobsOptions->setPrefix("HelloWorld");
+        // List blobs.
+        $listBlobsOptions = new ListBlobsOptions();
+        $listBlobsOptions->setPrefix("HelloWorld");
 
-//         echo "These are the blobs present in the container: ";
+        echo "These are the blobs present in the container: ";
 
-//         do{
-//             $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-//             foreach ($result->getBlobs() as $blob)
-//             {
-//                 echo $blob->getName().": ".$blob->getUrl()."<br />";
-//             }
+        do{
+            $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+            foreach ($result->getBlobs() as $blob)
+            {
+                echo $blob->getName().": ".$blob->getUrl()."<br />";
+            }
         
-//             $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-//         } while($result->getContinuationToken());
-//         echo "<br />";
+            $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+        } while($result->getContinuationToken());
+        echo "<br />";
 
-//         // Get blob.
-//         echo "This is the content of the blob uploaded: ";
-//         $blob = $blobClient->getBlob($containerName, $fileToUpload);
-//         fpassthru($blob->getContentStream());
-//         echo "<br />";
-//     }
-//     catch(ServiceException $e){
-//         // Handle exception based on error codes and messages.
-//         // Error codes and messages are here:
-//         // http://msdn.microsoft.com/library/azure/dd179439.aspx
-//         $code = $e->getCode();
-//         $error_message = $e->getMessage();
-//         echo $code.": ".$error_message."<br />";
-//     }
-//     catch(InvalidArgumentTypeException $e){
-//         // Handle exception based on error codes and messages.
-//         // Error codes and messages are here:
-//         // http://msdn.microsoft.com/library/azure/dd179439.aspx
-//         $code = $e->getCode();
-//         $error_message = $e->getMessage();
-//         echo $code.": ".$error_message."<br />";
-//     }
+        // Get blob.
+        echo "This is the content of the blob uploaded: ";
+        $blob = $blobClient->getBlob($containerName, $fileToUpload);
+        fpassthru($blob->getContentStream());
+        echo "<br />";
+    }
+    catch(ServiceException $e){
+        // Handle exception based on error codes and messages.
+        // Error codes and messages are here:
+        // http://msdn.microsoft.com/library/azure/dd179439.aspx
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code.": ".$error_message."<br />";
+    }
+    catch(InvalidArgumentTypeException $e){
+        // Handle exception based on error codes and messages.
+        // Error codes and messages are here:
+        // http://msdn.microsoft.com/library/azure/dd179439.aspx
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code.": ".$error_message."<br />";
+    }
 	// YQF2l8EOcMxCxZZMS2Qh4/a58DB3AAN32V5Yz/E8iLYdwhvJhZmMVj9CAwqUTdl0TZXL473Aw7xFaqZyj7YpfA==
 
 
-	$img = rand(1000,100000)."-".$_FILES['img']['name'];
-	$img_loc = $_FILES['img']['tmp_name'];
-	$folder="uploads/";
-	if(move_uploaded_file($img_loc,$folder.$img))
-	{
-		echo "<script>alert('Upload Sukses!!!');</script>";
-	}
-	else
-	{
-		echo "<script>alert('Upload Gagal');</script>";
-	} 
+	// $img = rand(1000,100000)."-".$_FILES['img']['name'];
+	// $img_loc = $_FILES['img']['tmp_name'];
+	// $folder="uploads/";
+	// if(move_uploaded_file($img_loc,$folder.$img))
+	// {
+	// 	echo "<script>alert('Upload Sukses!!!');</script>";
+	// }
+	// else
+	// {
+	// 	echo "<script>alert('Upload Gagal');</script>";
+	// } 
 }
 
 ?>

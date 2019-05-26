4ff1c7ac21d2219ab988ad313b79174e2fa1f73f
Agam Setiawan <agam.setiawan@wgs.co.id>
2019-05-26 12:20:43 +0700

    vision and file upload

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ if(isset($_POST['btn-upload']))
         echo "This is the content of the blob uploaded: ";
         $blob = $blobClient->getBlob($containerName, $fileToUpload);
         fpassthru($blob->getContentStream());
-        fpassthru($blob->getUrl())
+        fpassthru($blob->getUrl());
         echo "<br />";
 
         echo "<td><img src='' width='100' height='100'></td>";
